1a29a9dfbdd8a0355a7ebdcebe4e7655cac8a5d2
RitikaChourasiaa <ritika.chourasia0417@gmail.com>
2025-07-03 16:37:01 +0530

    Update heading Only

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import './App.css';
 const App = () => {
   return (
     <main className="container bg-light my-5 p-4  rounded-4 shadow" style={{maxWidth: "700px"}}>
-      <h1 className="app-title text-center text-dark fs-1 fw-bolder m-4">ToDo React + Typescript</h1>
+      <h1 className="app-title text-center text-dark fs-1 fw-bolder m-4">ToDo React js + Typescript</h1>
       <Navbar/>
       <AddTodo />
       <Todos/>
